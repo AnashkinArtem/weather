03adadc4bebbb033ec802fb24531f1ff58a3d167
Artem Anashkin <artem1anashkin@gmail.com>
2022-07-04 09:26:18 +0300

    update EventsData

--- src/data/EventsData.ts
+++ src/data/EventsData.ts
@@ -1,13 +1,13 @@
 const eventsData = [
     {
-        "dt": '2022-06-22',
+        "dt": '2022-07-04',
         'event': {
             '10:00': 'Read work emails',
             '14:00': 'Call parents'
         }       
     },
     {
-        "dt": '2022-06-23',
+        "dt": '2022-07-05',
         'event': {
             '16:00': 'Go to the store',
             '18:00': 'Meet Alex',
@@ -15,41 +15,41 @@ const eventsData = [
         } 
     },
     {
-        "dt": '2022-06-24',
+        "dt": '2022-07-06',
         'event': {
             '12:00': 'Zoo',
             '17:00': 'Football',
         } 
     },
     {
-        "dt": '2022-06-25',
+        "dt": '2022-07-07',
         'event': {
             '10:00': 'Studies',
             '18:00': 'Meeting with friends',
         } 
     },
     {
-        "dt": '2022-06-26',
+        "dt": '2022-07-08',
         'event': {
             '8:00': 'Walk the dog',
             '18:00': 'Visit grandfather',
         } 
     },
     {
-        "dt": '2022-06-27',
+        "dt": '2022-07-09',
         'event': {
             '16:00': 'Milk',
         } 
     },
     {
-        "dt": '2022-06-28',
+        "dt": '2022-07-10',
         'event': {
             '10:00': 'Courses',
             '20:00': 'French',
         } 
     },
     {
-        "dt": '2022-06-29',
+        "dt": '2022-07-11',
         'event': {
             '11:00': 'Gym',
             '13:00': 'Go to the store',
